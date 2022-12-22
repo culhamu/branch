42fdbd423a218eb94e51c4f8f001b76979ac6d14
Mustafa Çulha <your_merguven75@mail.com>
2022-12-22 14:13:05 +0300

    vi

---
+++ src/B3.java
@@ -0,0 +1,15 @@
+public class B3 {
+    public static void main(String[] args) {
+        /*
+        projemizde önemli bir değişiklik yapacağız
+        10 gün çalışmak zorundaysak
+        değişikliği yaparken orjinal projeyi riske atmamak için
+        ve eğer son kullanıcı kullanıyorsa oluşabilecek sorunları engellemek için
+
+
+        orjinal projede çalışmak yerine
+        yeni bir branch oluşturup
+        o branch i asıl brancha kopyalarız
+         */
+    }
+}
