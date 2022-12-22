ebedc60056a6d4e8eed83a11f05c9232c9c9a336
Mustafa Çulha <your_merguven75@mail.com>
2022-12-22 14:30:18 +0300

    çakışma öncesi son branch v8

---
+++ src/B7.java
@@ -0,0 +1,8 @@
+public class B7 {
+    public static void main(String[] args) {
+        /*
+        farklı branch lerde çalışırken kendi projemiz olduğu için farklı class larda çalışıp çakışma olmasını engelledik
+        ama bazen ister istemez çakışmalar olabilir
+         */
+    }
+}
