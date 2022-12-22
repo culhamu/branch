d409b99068e05c17b33a44b2928920c82ff6a89c
Mustafa Çulha <your_merguven75@mail.com>
2022-12-22 13:52:40 +0300

    iki günün özeti

---
+++ src/B2.java
@@ -0,0 +1,16 @@
+public class B2 {
+    public static void main(String[] args) {
+        /*
+        temelde git ve github için 3 aşamadan bahsedebiliriz
+        1 proje veya klasörümüzü git ile tanıştırma git add .
+        2 proje veya klasörümüz için versiyon oluşturma git commit -m"versiyon ismi"
+        3 gitGub ile entegrasyon ve senkronizasyon
+           -ilk defa entegrasyon etmek için github da yeni repo oluşturup
+           oradaki son 3 adımı kopyalayıp terminalde yapıştırıp enter lıyoruz
+           -ilk entegrasyondan sonra
+           1 git add .
+           2 git commit -m"versiyon ismi"
+           3 git push
+         */
+    }
+}
